5efd6bcd31e13d37aa9fea9b81452d401366da8b
mostlyclicks <carlos@mostlyclicks.com>
2013-11-19 13:55:44 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viroqua01::Application.config.secret_token = '9757ce4c5245ddfba26ae9c4a274a5596b5cc9db0b40b540d322821659e3a1e531cf7074a5d1105ccfeded6b0668a6f640b044ce2b953f8466d2977070459ed8'
